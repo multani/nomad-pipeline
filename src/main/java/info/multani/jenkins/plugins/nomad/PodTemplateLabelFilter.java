521d34e4b8ff3b2c4158c5a30bd131f69004d221
Jonathan Ballet <jon@multani.info>
2018-10-05 23:11:14 +0200

    Actually, that was useful

---
+++ src/main/java/info/multani/jenkins/plugins/nomad/PodTemplateLabelFilter.java
@@ -0,0 +1,23 @@
+package info.multani.jenkins.plugins.nomad;
+                                                                                                                                                                                                                                                                        
+
+import javax.annotation.CheckForNull;
+import javax.annotation.Nonnull;
+
+import hudson.Extension;
+import hudson.model.Label;
+import hudson.model.Node;
+
+/**
+ * Implementation of {@link PodTemplateFilter} filtering pod templates matching the right label.
+ */
+@Extension
+public class PodTemplateLabelFilter extends PodTemplateFilter {
+    @Override
+    protected NomadJobTemplate transform(@Nonnull NomadCloud cloud, @Nonnull NomadJobTemplate podTemplate, @CheckForNull Label label) {
+        if ((label == null && podTemplate.getNodeUsageMode() == Node.Mode.NORMAL) || (label != null && label.matches(podTemplate.getLabelSet()))) {
+            return podTemplate;
+        }
+        return null;
+    }
+}
